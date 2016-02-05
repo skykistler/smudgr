ac5fd897a1319d14414aa7883716b91fa4fe3f4f
ericvoorhis <eav1211@gmail.com>
2016-02-04 19:11:07 -0500

    Added Pixel Shift Edits

--- smudgr/src/io/smudgr/alg/PixelShift.java
+++ smudgr/src/io/smudgr/alg/PixelShift.java
@@ -7,6 +7,7 @@ import io.smudgr.alg.coord.ColumnCoords;
 import io.smudgr.alg.coord.CoordFunction;
 import io.smudgr.alg.math.LinearFunction;
 import io.smudgr.alg.math.UnivariateFunction;
+import io.smudgr.alg.param.BooleanParameter;
 import io.smudgr.alg.param.NumberParameter;
 import io.smudgr.source.Frame;
 
@@ -17,8 +18,9 @@ public class PixelShift extends Algorithm {
 	//private NumberParameter intervals = new NumberParameter(this, "Intervals", 0.25, 0, 0.51, 0.001);
 	//private NumberParameter start = new NumberParameter(this, "Start", 0.4, 0, 1, 0.001);
 	//private NumberParameter end = new NumberParameter(this, "End", 0.55, 0, 1, 0.001);
-	private NumberParameter shiftType = new NumberParameter(this, "Shift Type", 0, 0, 2, 1); // allows shift to be set via switch structure in calculateShift function
-	
+	private NumberParameter shiftType = new NumberParameter(this, "Shift Type", 1, 1, 3, 1); // allows shift to be set via switch structure in calculateShift function
+	private BooleanParameter reverse = new BooleanParameter(this, "Reverse", true);
+	private NumberParameter optional = new NumberParameter(this, "Optional", 100, 100, 10000, 10);
 	
 	UnivariateFunction scale = new LinearFunction();
 
@@ -41,19 +43,11 @@ public class PixelShift extends Algorithm {
 	public void execute(Frame img) {
 		int size = getCoordFunction().getCoordSet().size();
 		
-		//if (intervals.getMax() != size)
-		//	intervals.setMax(size);
-
 		orig = img.copy();
 		shifted = img.copy();
 
 		CoordFunction cf = getCoordFunction();
 		double shift = amount.getValue();
-		
-//		if (intervalWidth < 1) 
-//			intervalWidth = 1;
-//		else if (intervalWidth > size/2) 
-//			intervalWidth = size;
 		
 		double ints = intervals.getValue();
 		
@@ -77,7 +71,7 @@ public class PixelShift extends Algorithm {
 				int index = (int) (interval + i);
 				if (index < cf.getCoordSet().size()) {
 					ArrayList<Integer> coords = cf.getCoordSet().get(index);
-					shift(coords, n * shift);
+					shift(coords, n, shift, i, ints);
 				}
 			}
 		}
@@ -85,13 +79,45 @@ public class PixelShift extends Algorithm {
 		img.pixels = shifted.pixels;
 	}
 	
-	public void shift(ArrayList<Integer> coords, double amount) {
+	public void shift(ArrayList<Integer> coords, int currentInterval, double amount, int indexIntoCurrentInt, double totalInts) {
+		
+		double shiftScale = 1;
+		
+		//	find type of shift
+		
+		switch(shiftType.getIntValue()) {
+		
+		case 1:
+			shiftScale = currentInterval;
+			break;
+		case 2:
+			shiftScale = (currentInterval % 2 == 0 ? -currentInterval : currentInterval);
+			break;
+		case 3:
+			double sin = Math.sin( optional.getValue() * Math.toRadians(currentInterval/totalInts) );
+			shiftScale = sin;
+			break;
+		}
+		
+		//	reverse direction of shifting 
+		
+		if (reverse.getValue())
+			shiftScale = -shiftScale;
+		
+		
+		//	shift pixels now and wrap around using modulo
+		
 		for (int i = 0; i < coords.size(); i++) {
-			int shift = (int) (i + amount * coords.size());
-			shift %= coords.size();
-
+			
+			int shift = (int) (i + (shiftScale * amount) * coords.size());
+					
+			shift = Math.floorMod(shift, coords.size());
+			//shift %= coords.size();
+			
 			shifted.pixels[coords.get(i)] = orig.pixels[coords.get(shift)];
 		}
+		
+		
 	}
 
 	public boolean inStopArea(int n, int n0, int n1, int width){
