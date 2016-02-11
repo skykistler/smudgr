234f99edb5966ec2fe616ff38404c1159eaf107a
Sky <sky@smudgr.io>
2016-02-11 04:35:53 -0500

    no-wrap components and launch from shell directory

--- smudgr/ui/mac_launch.sh
+++ smudgr/ui/mac_launch.sh
@@ -1,1 +1,2 @@
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -app=file://$PWD/index.html
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -app=file://$DIR/index.html
