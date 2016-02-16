a73a83b4f1a49a21d2102634673899c551598bd9
Sky <sky@smudgr.io>
2016-02-16 16:27:37 -0500

    move some stuff around

--- smudgr/builds/macosx64/compile.sh
+++ smudgr/builds/macosx64/compile.sh
@@ -2,6 +2,6 @@ SHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SHDIR"
 
-cp -f ../../smudgr.jar "./bin/smudgr.app/Contents/Java/"
+cp -f ../smudgr.jar "./bin/smudgr.app/Contents/Java/"
 
 open ./bin/smudgr.app
