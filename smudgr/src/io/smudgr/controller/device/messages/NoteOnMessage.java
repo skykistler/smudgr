9219ac3aa2599995ba7f5510591266746c2476ee
Sky <sky@smudgr.io>
2016-02-25 21:44:25 -0500

    Closes #21. Thanks presto @coolhome

--- smudgr/src/io/smudgr/controller/device/messages/NoteOnMessage.java
+++ smudgr/src/io/smudgr/controller/device/messages/NoteOnMessage.java
@@ -5,7 +5,10 @@ import io.smudgr.controller.controls.Controllable;
 public class NoteOnMessage implements MidiMessageStrategy {
 
 	public void input(Controllable c, int value) {
-		c.inputOn(value);
+		if (value == 0)
+			c.inputOff(value);
+		else
+			c.inputOn(value);
 	}
 
 }
