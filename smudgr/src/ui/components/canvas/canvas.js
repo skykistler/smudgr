0467b981a8a9d57b553e57176c51c112b10c7f7f
Sky <sky@smudgr.io>
2016-04-12 12:13:59 -0400

    Canvas fix

--- smudgr/src/ui/components/canvas/canvas.js
+++ smudgr/src/ui/components/canvas/canvas.js
@@ -1,33 +1,19 @@
 function CanvasCtrl($scope, $http, smudgr, $document) {
-  $scope.image = [];
-
-  $scope.canvasX = 0;
-  $scope.canvasY = 0;
-  $scope.canvasWidth = 0;
-  $scope.canvasHeight = 0;
-
-  $scope.canvasSizeChanged = function() {
+  
+  var canvasSizeChanged = function() {
     var canvas = $document.find("smudgr-canvas");
 
-    var x = parseInt(canvas.offset().left);
-    var y = parseInt(canvas.offset().top);
-    var w = parseInt(canvas.width());
-    var h = parseInt(canvas.height());
-
-    if (x == $scope.canvasX && y == $scope.canvasY && w == $scope.canvasWidth && h == $scope.canvasHeight)
-      return false;
-
-    $scope.canvasX = x;
-    $scope.canvasY = y;
-    $scope.canvasWidth = w;
-    $scope.canvasHeight = h;
-
-    return true;
+    return {
+      x : parseInt(canvas.offset().left),
+      y : parseInt(canvas.offset().top),
+      w : parseInt(canvas.width()),
+      h : parseInt(canvas.height())
+    };
   }
 
-  $scope.$watch('canvasSizeChanged()', function(newValue, oldValue) {
-    smudgr.exec("canvas.size", [$scope.canvasX, $scope.canvasY, $scope.canvasWidth, $scope.canvasHeight] );
-  });
+  $scope.$watch(canvasSizeChanged, function(newValue, oldValue) {
+    smudgr.exec("canvas.size", [newValue.x, newValue.y, newValue.w, newValue.h] );
+  }, true);
 }
 
 angular.module('smudgr').component('smudgrCanvas', {
