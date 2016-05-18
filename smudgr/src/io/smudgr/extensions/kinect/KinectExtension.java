40486cbdbc93a3a8a17da2fbbca54ebfdf7d044a
ericvoorhis <eav1211@gmail.com>
2016-05-18 15:43:19 -0400

    Fixed kinect extension when not loaded

--- smudgr/src/io/smudgr/extensions/kinect/KinectExtension.java
+++ smudgr/src/io/smudgr/extensions/kinect/KinectExtension.java
@@ -69,6 +69,9 @@ public class KinectExtension implements ControllerExtension {
 
 	@Override
 	public void stop() {
+		if (device == null)
+			return;
+
 		device.stopDepth();
 		device.close();
 	}
