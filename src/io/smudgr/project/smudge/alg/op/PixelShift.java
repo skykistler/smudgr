d5bf128115894ef1cf4cb4ac1de5b62749e26e7a
ericvoorhis <eric.voorhis14@ncf.edu>
2016-11-03 23:37:59 -0400

    Removed 'optional' Parameter and added 2 shift types

--- src/io/smudgr/project/smudge/alg/op/PixelShift.java
+++ src/io/smudgr/project/smudge/alg/op/PixelShift.java
@@ -12,14 +12,21 @@ public class PixelShift extends Operation {
 	}
 
 	private NumberParameter amount = new NumberParameter("Amount", this, 0, 0, 1, 0.005);
-	private NumberParameter intervals = new NumberParameter("Intervals", this, 5, 1, 1000, 1); // Change to be a percentage or not I guess
+	private NumberParameter intervals = new NumberParameter("Intervals", this, 5, 1, 1000, 1);
+
 	private NumberParameter start = new NumberParameter("Start", this, 0, 0, 1, 0.01);
 	private NumberParameter length = new NumberParameter("Length", this, 1, 0, 1, 0.01);
-	private NumberParameter shiftType = new NumberParameter("Shift Type", this, 1, 1, 3, 1); // allows shift to be set via switch structure in calculateShift function
-	private BooleanParameter reverse = new BooleanParameter("Reverse", this, true);
-	private NumberParameter optional = new NumberParameter("Optional", this, 100, 100, 10000, 10);
 
-	//	private UnivariateParameter scale = new UnivariateParameter("Scale", this, new LinearFunction());
+	/*
+	 * allows shift to be set via switch structure in calculateShift function
+	 */
+	private NumberParameter shiftType = new NumberParameter("Shift Type", this, 1, 1, 4, 1);
+	private BooleanParameter reverse = new BooleanParameter("Reverse", this, true);
+
+	private int selectedShiftType;
+	private boolean isReversed;
+	private int intervalStart;
+	private int intervalEnd;
 
 	public void init() {
 		amount.setContinuous(true);
@@ -27,6 +34,9 @@ public class PixelShift extends Operation {
 
 	public void execute(Frame img) {
 		int size = getAlgorithm().getSelectedPixels().size();
+
+		selectedShiftType = shiftType.getIntValue();
+		isReversed = reverse.getValue();
 
 		Frame shifted = img.copy();
 
@@ -49,6 +59,9 @@ public class PixelShift extends Operation {
 			ints = size;
 		}
 
+		intervalStart = n0;
+		intervalEnd = n1;
+
 		for (int n = n0 + 1; n < n1 + 1; n++) {
 
 			double interval = (n - 1) * intervalWidth;
@@ -66,13 +79,14 @@ public class PixelShift extends Operation {
 		shifted.dispose();
 	}
 
-	public void shift(Frame shifted, Frame orig, PixelIndexList coords, int currentInterval, double amount, int indexIntoCurrentInt, double totalInts) {
+	public void shift(Frame shifted, Frame orig, PixelIndexList coords, int currentInterval, double amount,
+			int indexIntoCurrentInt, double totalInts) {
 
 		double shiftScale = 1;
 
-		//	find type of shift
+		// find type of shift
 
-		switch (shiftType.getIntValue()) {
+		switch (selectedShiftType) {
 
 		case 1:
 			shiftScale = currentInterval;
@@ -81,17 +95,22 @@ public class PixelShift extends Operation {
 			shiftScale = (currentInterval % 2 == 0 ? -currentInterval : currentInterval);
 			break;
 		case 3:
-			double sin = Math.sin(optional.getValue() * Math.toRadians(currentInterval / totalInts));
-			shiftScale = sin;
+			int w = intervalEnd - intervalStart;
+			shiftScale = Math.abs((intervalStart + (w / 2)) - currentInterval + 1);
+			break;
+		case 4:
+			w = intervalEnd - intervalStart;
+			shiftScale = Math.abs((intervalStart + (w / 2)) - currentInterval + 1);
+			shiftScale = (currentInterval % 2 == 0 ? -shiftScale : shiftScale);
 			break;
 		}
 
-		//	reverse direction of shifting 
+		// reverse direction of shifting
 
-		if (reverse.getValue())
+		if (isReversed)
 			shiftScale = -shiftScale;
 
-		//	shift pixels now and wrap around using modulo
+		// shift pixels now and wrap around using modulo
 
 		for (int i = 0; i < coords.size(); i++) {
 			int shift = (int) (i + (shiftScale * amount) * coords.size());
---
+++ src/io/smudgr/test/PixelShiftTest.java
@@ -0,0 +1,77 @@
+package io.smudgr.test;
+
+import io.smudgr.app.AppStart;
+import io.smudgr.app.Controller;
+import io.smudgr.project.smudge.Smudge;
+import io.smudgr.project.smudge.alg.Algorithm;
+import io.smudgr.project.smudge.alg.coord.RowCoords;
+import io.smudgr.project.smudge.alg.op.PixelShift;
+
+public class PixelShiftTest extends AppStart {
+
+	/*
+	 * To make your own test class, just duplicate this file and change the
+	 * following
+	 */
+
+	// where to save/load project file
+	static String projectPath = "data/test.smudge";
+
+	// whether to overwrite the existing project file
+	static boolean overwriteSmudge = true;
+
+	// where to load source files from
+	static String sourcePath = "data/work";
+
+	// where to output gifs and stuff
+	static String outputPath = "data/output";
+
+	// name of device to bind to
+	static String device = "Arturia BeatStep";
+
+	// whether to start a server to broadcast midi signals
+	static boolean deviceServer = false;
+
+	// set to -1 for no fullscreen, set to 0 for fullscreen on main display
+	// Higher numbers are for multi-monitor setups
+	static int fullscreenDisplay = -1;
+
+	// non-fullscreen window
+	static boolean monitor = true;
+
+	public void buildSmudge() {
+		Smudge smudge = Controller.getInstance().getProject().getSmudge();
+
+		Algorithm alg = new Algorithm();
+
+		PixelShift shift = new PixelShift();
+		alg.add(shift);
+		alg.add(new RowCoords());
+		smudge.add(alg);
+
+		shift.getParameter("Intervals").setContinuous(true);
+		shift.getParameter("Shift Type").setValue(3);
+
+		shift.getParameter("Intervals").setValue(20);
+		addAutomator("Animate", shift.getParameter("Amount"));
+		// addAutomator("Animate", shift.getParameter("Intervals"));
+
+		smudge.getParameter("Downsample").setValue(1);
+
+	}
+
+	public PixelShiftTest() {
+		super(projectPath, sourcePath, outputPath, device, overwriteSmudge, deviceServer);
+
+		fullscreenView(fullscreenDisplay);
+		if (monitor)
+			monitorView();
+
+		start();
+	}
+
+	public static void main(String[] args) {
+		new PixelShiftTest();
+	}
+
+}
