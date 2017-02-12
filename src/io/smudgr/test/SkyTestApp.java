b426d33ff215733b6392361263a8065e716c2f12
Sky Kistler <sky@smudgr.io>
2017-02-11 19:25:39 -0500

    update tests

--- src/io/smudgr/test/SkyTestApp.java
+++ src/io/smudgr/test/SkyTestApp.java
@@ -4,19 +4,20 @@ import io.smudgr.app.AppStart;
 import io.smudgr.app.controller.Controller;
 import io.smudgr.engine.Smudge;
 import io.smudgr.engine.alg.Algorithm;
-import io.smudgr.engine.alg.coord.ConvergeCoords;
-import io.smudgr.engine.alg.coord.StraightCoords;
 import io.smudgr.engine.alg.op.ChannelBleed;
+import io.smudgr.engine.alg.op.ChannelCrush;
+import io.smudgr.engine.alg.op.ChannelDrift;
 import io.smudgr.engine.alg.op.ChannelSort;
 import io.smudgr.engine.alg.op.DataBend;
+import io.smudgr.engine.alg.op.HSVLModifier;
 import io.smudgr.engine.alg.op.Marbeler;
 import io.smudgr.engine.alg.op.Operation;
 import io.smudgr.engine.alg.op.ParticlePush;
 import io.smudgr.engine.alg.op.PixelShift;
 import io.smudgr.engine.alg.op.PixelSort;
+import io.smudgr.engine.alg.op.RainbowBend;
+import io.smudgr.engine.alg.op.Smear;
 import io.smudgr.engine.alg.op.SpectralShift;
-import io.smudgr.engine.alg.select.RangeSelect;
-import io.smudgr.engine.param.NumberParameter;
 import io.smudgr.extensions.cef.view.WebsocketView;
 
 /**
@@ -61,9 +62,19 @@ public class SkyTestApp extends AppStart {
 	public void buildSmudge() {
 		Smudge smudge = Controller.getInstance().getProject().getSmudge();
 
-		getOperationAlgorithm(smudge, new ParticlePush());
-		addStraightPixelSort(smudge);
+		getOperationAlgorithm(smudge, new ChannelBleed());
+		getOperationAlgorithm(smudge, new ChannelCrush());
+		getOperationAlgorithm(smudge, new ChannelDrift());
 		getOperationAlgorithm(smudge, new ChannelSort());
+		getOperationAlgorithm(smudge, new DataBend());
+		getOperationAlgorithm(smudge, new HSVLModifier());
+		getOperationAlgorithm(smudge, new Marbeler());
+		getOperationAlgorithm(smudge, new ParticlePush());
+		getOperationAlgorithm(smudge, new PixelShift());
+		getOperationAlgorithm(smudge, new PixelSort());
+		getOperationAlgorithm(smudge, new RainbowBend());
+		getOperationAlgorithm(smudge, new Smear());
+		getOperationAlgorithm(smudge, new SpectralShift());
 
 		bind(smudge.getParameter("Source Speed"));
 		bind(smudge.getParameter("Downsample"));
@@ -71,160 +82,6 @@ public class SkyTestApp extends AppStart {
 		bind(Controller.getInstance().getAppControl("Source Switcher"));
 		bind(Controller.getInstance().getAppControl("Source Set Switcher"));
 		bind(Controller.getInstance().getAppControl("Save Project"));
-	}
-
-	/**
-	 * @param smudge
-	 *            Smudge
-	 * @param columns
-	 *            boolean
-	 */
-	public void addStraightPixelShift(Smudge smudge, boolean columns) {
-		PixelShift pixel_shift = new PixelShift();
-		Algorithm shift_alg = getOperationAlgorithm(smudge, pixel_shift);
-
-		StraightCoords shift_coords = new StraightCoords();
-		shift_alg.add(shift_coords);
-		shift_coords.getParameter("Vertical").setValue(columns);
-		shift_coords.getParameter("Continuous").setValue(false);
-
-		bind(pixel_shift.getParameter("Reverse"));
-		bind(pixel_shift.getParameter("Intervals"));
-		bind(addAutomator("Animate", pixel_shift.getParameter("Amount")));
-	}
-
-	/**
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addConvergePixelShift(Smudge smudge) {
-		PixelShift pixel_shift = new PixelShift();
-		Algorithm shift_alg = getOperationAlgorithm(smudge, pixel_shift);
-
-		ConvergeCoords shift_coords = new ConvergeCoords();
-		shift_alg.add(shift_coords);
-		shift_coords.getParameter("Continuous").setValue(false);
-
-		bind(pixel_shift.getParameter("Reverse"));
-		bind(pixel_shift.getParameter("Intervals"));
-		bind(addAutomator("Animate", pixel_shift.getParameter("Amount")));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addStraightPixelSort(Smudge smudge) {
-		PixelSort sort = new PixelSort();
-		Algorithm alg = getOperationAlgorithm(smudge, sort);
-
-		alg.getParameter("Enable").setValue(true);
-
-		RangeSelect pixelsort_range = new RangeSelect();
-		alg.add(pixelsort_range);
-		pixelsort_range.getParameter("Range Length").setValue(1);
-
-		StraightCoords coords = new StraightCoords();
-		alg.add(coords);
-		coords.getParameter("Vertical").setValue(true);
-		coords.getParameter("Continuous").setValue(false);
-
-		bind(pixelsort_range.getParameter("Range Length"));
-		bind(sort.getParameter("Reverse"));
-		bind(coords.getParameter("Vertical"));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addConvergePixelSort(Smudge smudge) {
-		PixelSort sort = new PixelSort();
-		Algorithm alg = getOperationAlgorithm(smudge, sort);
-
-		RangeSelect pixelsort_range = new RangeSelect();
-		alg.add(pixelsort_range);
-		pixelsort_range.getParameter("Range Length").setValue(0);
-
-		ConvergeCoords coords = new ConvergeCoords();
-		alg.add(coords);
-		coords.getParameter("Continuous").setValue(false);
-
-		bind(pixelsort_range.getParameter("Range Length"));
-		bind(sort.getParameter("Reverse"));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addDatabend(Smudge smudge) {
-		DataBend databend = new DataBend();
-		databend.getParameter("Amount").setValue(2);
-
-		Algorithm alg = getOperationAlgorithm(smudge, databend);
-
-		StraightCoords coords = new StraightCoords();
-		alg.add(coords);
-		coords.getParameter("Vertical").setValue(true);
-		coords.getParameter("Continuous").setValue(false);
-
-		RangeSelect databend_range = new RangeSelect();
-		databend_range.getParameter("Range Length").setValue(1);
-		alg.add(databend_range);
-
-		bind(addAutomator("Animate", databend.getParameter("Target")));
-		bind(databend.getParameter("Amount"));
-		bind(coords.getParameter("Vertical"));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addSpectralShift(Smudge smudge) {
-		SpectralShift spectral_shift = new SpectralShift();
-		getOperationAlgorithm(smudge, spectral_shift);
-
-		spectral_shift.getParameter("Colors").setValue(6);
-
-		bind(addAutomator("Beat Sync", spectral_shift.getParameter("Shift")));
-
-		bind(spectral_shift.getParameter("Colors"));
-		bind(spectral_shift.getParameter("Palette"));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addChannelBleed(Smudge smudge) {
-		ChannelBleed bleed = new ChannelBleed();
-		((NumberParameter) bleed.getParameter("Shift Amount")).setContinuous(true);
-
-		getOperationAlgorithm(smudge, bleed);
-
-		bind(bleed.getParameter("Shift Amount"));
-	}
-
-	/**
-	 *
-	 * @param smudge
-	 *            Smudge
-	 */
-	public void addMarbeler(Smudge smudge) {
-		Marbeler marb = new Marbeler();
-		getOperationAlgorithm(smudge, marb);
-
-		bind(marb.getParameter("Frequency"));
-		bind(marb.getParameter("Iterations"));
-		bind(marb.getParameter("Strength"));
-		// bind(marb.getParameter("Seed"));
 	}
 
 	/**
